efe1529fe54e6c3d5b4beeb3352caefd3c6119dc
Denys Zhuravel <denys.zhuravel@albelli.com>
2019-04-05 17:01:06 +0200

    remove useless annotations

--- src/main/kotlin/albelli/junit/synnefo/api/SynnefoOptions.kt
+++ src/main/kotlin/albelli/junit/synnefo/api/SynnefoOptions.kt
@@ -7,7 +7,6 @@ import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 annotation class SynnefoOptions(
 
         /**
