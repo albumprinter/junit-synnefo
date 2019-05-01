4758b69528600e53fffef6eb2da3bf90bf03fda6
Denys Zhuravel <denys.zhuravel@albelli.com>
2019-05-01 10:37:20 +0200

    Add cucumber forced tags

--- src/main/kotlin/albelli/junit/synnefo/runtime/SynnefoRuntimeOptionsCreator.kt
+++ src/main/kotlin/albelli/junit/synnefo/runtime/SynnefoRuntimeOptionsCreator.kt
@@ -1,10 +1,10 @@
 package albelli.junit.synnefo.runtime
 
 import cucumber.api.CucumberOptions
-import kotlin.collections.ArrayList
 
 internal class SynnefoRuntimeOptionsCreator(synnefoProperties: SynnefoProperties) {
     private val cucumberOptions: CucumberOptions = synnefoProperties.cucumberOptions
+    private val cucumberForcedTags: String = synnefoProperties.cucumberForcedTags
     private val runtimeOptions = ArrayList<String>()
 
     init {
@@ -18,8 +18,13 @@ internal class SynnefoRuntimeOptionsCreator(synnefoProperties: SynnefoProperties
     private fun createRuntimeOptions(cucumberOptions: CucumberOptions): List<String> {
         val runtimeOptions = ArrayList<String>()
 
+        val tags = envCucumberOptionOverride("tags", cucumberOptions.tags.toList()).map {
+            if(cucumberForcedTags.isNullOrWhiteSpace()) it
+            else "($it) AND ($cucumberForcedTags)"
+        }
+        runtimeOptions.addAll(optionParser("--tags", tags))
+
         runtimeOptions.addAll(optionParser("--glue", envCucumberOptionOverride("glue", cucumberOptions.glue.toList())))
-        runtimeOptions.addAll(optionParser("--tags", envCucumberOptionOverride("tags", cucumberOptions.tags.toList())))
         runtimeOptions.addAll(optionParser("--plugin", envCucumberOptionOverride("plugin", cucumberOptions.plugin.toList())))
         runtimeOptions.addAll(optionParser("--name", envCucumberOptionOverride("name", cucumberOptions.name.toList())))
         runtimeOptions.addAll(optionParser("--junit", envCucumberOptionOverride("junit", cucumberOptions.junit.toList())))
