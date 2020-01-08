4e6a96f36a7e6faf76061d0e143500e34b72e914
Denys Zhuravel <denys.zhuravel@albelli.com>
2020-01-08 12:13:36 +0100

    Use an empty array as a default value for codeBuildRunTimeVersions

--- src/main/kotlin/albelli/junit/synnefo/api/SynnefoOptions.kt
+++ src/main/kotlin/albelli/junit/synnefo/api/SynnefoOptions.kt
@@ -87,7 +87,7 @@ annotation class SynnefoOptions(
          * @return a set of CodeBuild runtimes to initialize.
          * See the docu for details: https://docs.aws.amazon.com/codebuild/latest/userguide/sample-runtime-versions.html
          */
-        val codeBuildRunTimeVersions: Array<String>
+        val codeBuildRunTimeVersions: Array<String> = []
         )
 @Retention(RetentionPolicy.RUNTIME)
 @Target(AnnotationTarget.CLASS)
